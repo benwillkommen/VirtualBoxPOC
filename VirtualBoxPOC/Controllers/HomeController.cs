4a98820109614910561a695e7a05d9b5fc902c1a
ben willkommen <benwillkommen@gmail.com>
2014-04-22 13:13:15 -0500

    first commit

---
+++ VirtualBoxPOC/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VirtualBoxPOC.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View(new FileModel());
+        }
+
+        [HttpPost]
+        public ActionResult Index(FileModel model)
+        {
+            model.Contents = System.IO.File.ReadAllText(model.Path);
+            return View(model);
+        }
+
+    }
+
+    public class FileModel
+    {
+        public string Path { get; set; }
+        public string Contents { get; set; }
+    }
+}
---
+++ VirtualBoxPOC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VirtualBoxPOC.MvcApplication" Language="C#" %>
